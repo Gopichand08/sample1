9980e68d8ce3b05de836cf6c4b776684f51e93c3
Gopichand08 <gopi28.gc@gmail.com>
2017-06-24 08:25:28 +0530

    Add files via upload

---
+++ log-in.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    if (confirm("Do you already have a account") == true) {
+        window.location="file:///C:/bootstrap-3.3.7-dist/login.html";
+    } else {
+        
+        window.location="file:///C:/bootstrap-3.3.7-dist/register.html";
+    }
+    
+}
